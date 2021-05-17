7687a68d1d010371a36e4ce64e7b01c282b75d26
Reese <arter.charise@gmail.com>
2021-05-17 11:20:29 -0500

    cleaned up more files and initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-			</header>
+
 			<h1> Github Finder Redo </h1>
 		</div>
 	);
